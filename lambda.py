d7a0846ce3750356353cc327578c71ad29830818
Karthikeyan Chinnadurai <40652864+ckrdp@users.noreply.github.com>
2023-09-19 00:24:31 +0530

    lambda.py

--- lambda.py
+++ lambda.py
@@ -1,1 +1,19 @@
-
+import json
+import boto3
+from time import gmtime, strftime
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Tempconv')
+now = strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+def lambda_handler(event, context):
+    F = int(event['celsius']) * 9 / 5 + 32;
+    K = int(event['celsius']) + 273.15;
+    response = table.put_item(
+        Item={
+            'Fvalue': str(F),
+	          'Kvalue': str(K),
+            'LatestGreetingTime':now
+            })
+    return {
+    'statusCode': 200,
+    'body': json.dumps('Farhenheit :' + str(F) + 'Kelvin:' + str(K))
+    }
