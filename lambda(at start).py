2200beda3799ddaf9ee5e6913e366d28f1e672d1
Karthikeyan Chinnadurai <40652864+ckrdp@users.noreply.github.com>
2023-09-19 00:22:51 +0530

    lambda(at start).py

--- lambda(at start).py
+++ lambda(at start).py
@@ -1,1 +1,8 @@
-
+import json
+def lambda_handler(event, context):
+    F = int(event['celsius']) * 9 / 5 + 32;
+    K = int(event['celsius']) + 273.15;
+    return {
+    'statusCode': 200,
+    'body': json.dumps('Farhenheit :' + str(F) + 'Kelvin:' + str(K))
+    }
